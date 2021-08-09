a36ec6e89ec16e56db86b2021b50867a4fb6a99d
Austin <austinm2040@gmail.com>
2021-08-09 16:22:23 -0400

    problem complete

---
+++ 2 Medium/lengthOfLongestSubstring.js
@@ -0,0 +1,65 @@
+/*
+
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Example 4:
+
+Input: s = ""
+Output: 0
+
+Constraints:
+
+0 <= s.length <= 5 * 10^4
+s consists of English letters, digits, symbols and spaces.
+
+*/
+
+/*
+edge case: string length 0 or 1 --> return length
+create obj to keep track of characters
+
+
+*/
+
+const lengthOfLongestSubstring = (s) => {
+  let chars = {};
+  let left = 0;
+  let right = 0;
+  let result = 0;
+
+  while (right < s.length) {
+    let charRight = s.charAt(right);
+    if (chars[charRight] === undefined) {
+      chars[charRight] = 1;
+    } else {
+      chars[charRight]++;
+    }
+
+    while (chars[charRight] > 1) {
+      let charLeft = s.charAt(left);
+      chars[charLeft]--;
+      left++;
+    }
+    result = Math.max(result, right - left + 1);
+    right++;
+  }
+  return result;
+};
