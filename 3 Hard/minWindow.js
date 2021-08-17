8bd1b0b57bd6b71448658ab33f4f7a05afdb8ba6
Austin <austinm2040@gmail.com>
2021-08-17 11:40:08 -0400

    code clean up

--- 3 Hard/minWindow.js
+++ 3 Hard/minWindow.js
@@ -36,15 +36,34 @@ s and t consist of uppercase and lowercase English letters.
 
 */
 
+/*
+create object to track frequency of all letters in t
+create right and left pointer, start at 0; count variable set = n
+create min variable to record lowest substring (right - left pointer values)
+create head variable = 0 --> will be start of substring
+while right < m
+ if s[right] is in obj, reduce s[right] by 1
+ if s[right] is in obj tracker & > 0, decrease count by 1
+ increase right pointer by 1
+ while count = 0
+  if right - left < min --> Number.MAX_SAFE_INTEGER
+   update min = right - left
+   update head = left
+  if s[left] is in obj tracker, increase s[left] by 1
+  if s[left] is in obj tracker & = 0, increase count by 1
+  increase left pointer by 1
+ return ternary: min === Number.MAX_SAFE_INTEGER ? "" : s.substr(head, min)
+*/
+
 const minWindow = (s, t) => {
   let charTracker = {};
   let m = s.length;
   let n = t.length;
+  let right = 0;
+  let left = 0;
   let count = n;
   let min = Number.MAX_SAFE_INTEGER;
   let head = 0;
-  let left = 0;
-  let right = 0;
 
   for (let i = 0; i < n; i++) {
     if (charTracker[t[i]] === undefined) {
