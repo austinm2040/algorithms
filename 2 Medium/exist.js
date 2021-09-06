de97d73ecc651b4e298fa26290d4b3e177a05b0a
Austin <austinm2040@gmail.com>
2021-09-06 09:24:45 -0400

    code clean up

--- 2 Medium/exist.js
+++ 2 Medium/exist.js
@@ -35,24 +35,40 @@ n = board[i].length
 1 <= m, n <= 6
 1 <= word.length <= 15
 board and word consists of only lowercase and uppercase English letters.
-
 */
 
 /*
-Start [0,0]
-if curr [i][j] === word[k] --> replace with symbol so as not to duplicate
-check up, left, down, right when able
-if adjacent !== word[k] go back to [i][j]
-if no adjacent cells = word[k], use regression to start anew, at [i][j + 1]
+Need Helper function to check left, right, up, down at each cell
+
+Helper:
+Pass in parameters board, word, i & j indices, k --> tracking char in word
+Check if
+  k === word.length --> if so, word has been found, return true
+  i < 0 or i >= m ---> i index out of bounds, need to recurse, return false
+  j < 0 or j >= n ---> j index out of bounds, need to recurse, return false
+  board[i][j] !== word[k] --> need to recurse, return false
+Else
+  Set result variable = false
+  Set char variable = board[i][j] --> need to store in case of recursion later
+  Need to replace board[i][j] with arbitrary non-letter so as not to re-use
+    board[i][j] = '#'
+  Use recursion with result to try to advance and find word (else move i, j)
+  Possible moves:
+    i - 1, j --> up
+    i + 1, j --> down
+    i, j - 1 --> left
+    i, j + 1 --> right
+    Always advance k --> k + 1 since has passed previous tests and not returned false
+  If result === false, reset board[i][j] to orig char, not '#'
+
+Main Function:
+Double For Loop
+  Use helper to check if word can be found starting at current index
 */
 
 const exist = (board, word) => {
-  const len1 = board.length;
-  const len2 = (board[0] || []).length;
-  const len3 = word.length;
-
-  for (let i = 0; i < len1; i++) {
-    for (let j = 0; j < len2; j++) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
       if (existHelper(board, word, i, j, 0)) {
         return true;
       }
@@ -61,32 +77,68 @@ const exist = (board, word) => {
   return false;
 };
 
-const existHelper = (board, word, m, n, k) => {
+const existHelper = (board, word, i, j, k) => {
   if (k === word.length) {
     return true;
   }
-  if (m < 0 || m >= board.length) {
+  if (i < 0 || i >= board.length) {
     return false;
   }
-  if (n < 0 || n >= board[m].length) {
+  if (j < 0 || j >= board[i].length) {
     return false;
   }
-  if (board[m][n] !== word[k]) {
+  if (board[i][j] !== word[k]) {
     return false;
   }
   let result = false;
-  const char = board[m][n];
+  const char = board[i][j];
 
-  board[m][n] = '#';
+  board[i][j] = '#';
 
-  result = existHelper(board, word, m - 1, n, k + 1)
-        || existHelper(board, word, m + 1, n, k + 1)
-        || existHelper(board, word, m, n - 1, k + 1)
-        || existHelper(board, word, m, n + 1, k + 1);
+  result = existHelper(board, word, i - 1, j, k + 1) // look up
+        || existHelper(board, word, i + 1, j, k + 1) // look down
+        || existHelper(board, word, i, j - 1, k + 1) // look left
+        || existHelper(board, word, i, j + 1, k + 1); // look right
 
   if (!result) {
-    board[m][n] = char;
+    board[i][j] = char;
   }
 
   return result;
 };
+
+
+/*
+Input: board = [["A","B","C","E"],
+                ["S","F","C","S"],
+                ["A","D","E","E"]],
+word = "ABCCED"
+
+Steps to find 'ABCCED':
+
+[['#','B','C','E'],
+ ['S','F','C','S'],
+ ['A','D','E','E']]
+
+[['#','#','C','E'],
+ ['S','F','C','S'],
+ ['A','D','E','E']]
+
+[['#','#','#','E'],
+ ['S','F','C','S'],
+ ['A','D','E','E']]
+
+[['#','#','#','E'],
+['S','F','#','S'],
+['A','D','E','E']]
+
+[['#','#','#','E'],
+ ['S','F','#','S'],
+ ['A','D','#','E']]
+
+[['#','#','#','E'],
+ ['S','F','#','S'],
+ ['A','#','#','E']]
+
+Output: true
+*/
