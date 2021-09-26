02ed111ff0da80d6c0c0e9664d59d16d82044ed9
Austin <austinm2040@gmail.com>
2021-09-26 16:58:36 -0400

    problem complete

--- 2 Medium/threeSum.js
+++ 2 Medium/threeSum.js
@@ -24,10 +24,59 @@ Constraints:
 */
 
 /*
+Can have duplicate numbers, only if all in original nums array
+No duplicate triplets
 
+First sort nums array
+track all triplets in array
+
+For loop --> 0 <= i < nums.length - 2
+  Check that triplets array will not have a duplicate
+  if (nums[i] !== nums[i + 1])
+    declare left & right pointer variables -> i = i + 1, right = nums.length - 1
+  While loop --> while left < right
+    Declare currentSum = sum of (nums[i] + nums[left] + nums[right])
+    if currentSum === 0 --> push to triplet array
+    Ensure triplet array does not contain duplicates
+      While loop --> nums[left] === nums[left + 1]
+        increase left by 1
+      While loop --> nums[right] === nums[right - 1]
+        decrease right by 1
+    Out of while loops --> Increase left by 1, decrease right by 1
+    Check if currentSum < 0 --> increase right by 1
+    otherwise, decrease right by 1
+Return triplet array
 */
 
-
 const threeSum = (nums) => {
+  nums.sort((a,b) => a - b);
+  const len = nums.length;
+  const triplets = [];
 
+  for (let i = 0; i < len - 2; i++) {
+    if (nums[i] !== nums[i - 1]) { // 
+    let left = i + 1;
+    let right = len - 1;
+
+    while (left < right) {
+      const currentSum = nums[i] + nums[left] + nums[right];
+      if (currentSum === 0) {
+        triplets.push([nums[i], nums[left], nums[right]]);
+        while (nums[left] === nums[left + 1]) {
+          left++;
+        }
+        while (nums[right] === nums[right - 1]) {
+          right--;
+        }
+        left++;
+        right--;
+        } else if (currentSum < 0) {
+          left++;
+        } else if (currentSum > 0) {
+          right--;
+        }
+      }
+    }
+  }
+  return triplets;
 };
